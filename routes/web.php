3be187743a30ba353368e62abd5c6b274cc89e3b
dhavalpatel11111 <dp715953@gmail.com>
2024-02-20 14:06:06 +0530

    frontend done

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,34 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Frontend.index');
+});
+
+
+Route::get('/shop', function () {
+    return view('Frontend.shop');
+});
+
+Route::get('/about', function () {
+    return view('Frontend.about');
+});
+
+Route::get('/services', function () {
+    return view('Frontend.services');
+});
+
+Route::get('/blog', function () {
+    return view('Frontend.blog');
+});
+
+Route::get('/contact', function () {
+    return view('Frontend.contact');
+});
+
+Route::get('/cart', function () {
+    return view('Frontend.cart');
+});
+
+Route::get('/checkout', function () {
+    return view('Frontend.checkout');
 });
